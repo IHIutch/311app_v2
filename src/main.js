3ccede1927d8fbf9ca1fb9acf394a200c83a993a
IHIutch <jbhutch01@gmail.com>
2019-07-21 09:53:43 -0400

    Install bootstrap, service-worker, prettier, and setup scss folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./registerServiceWorker";
+import App from "@/App.vue";
+import router from "@/router";
+import BootstrapVue from "bootstrap-vue";
+import "@/assets/scss/main.scss";
+
+Vue.use(BootstrapVue);
+import "@/registerServiceWorker";
 
 Vue.config.productionTip = false;
 
