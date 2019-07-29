29eeabf8cb1febf3c963aeaf09d52210a3b7934f
IHIutch <jbhutch01@gmail.com>
2019-07-29 12:15:08 -0400

    Import sentry

--- src/main.js
+++ src/main.js
@@ -5,13 +5,23 @@ import App from "@/App.vue";
 import router from "@/router";
 import BootstrapVue from "bootstrap-vue";
 import { firestorePlugin } from "vuefire";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
+// Global styles
 import "@/assets/scss/main.scss";
+
+// Enable PWA
+import "@/registerServiceWorker";
+
+// Initialize Sentry (Live error tracking)
+Sentry.init({
+  dsn: "https://fb0586e37cde4cbaa306926bf377ef5f@sentry.io/1516230",
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
 
 Vue.use(firestorePlugin);
 Vue.use(BootstrapVue);
-
-import "@/registerServiceWorker";
 
 Vue.config.productionTip = false;
 
