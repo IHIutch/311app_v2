9c9e1f591ba828d4f0cc671de8ed12d4b136c19c
IHIutch <jbhutch01@gmail.com>
2020-03-28 16:21:30 -0400

    Cleanup form

--- src/views/FormPage.vue
+++ src/views/FormPage.vue
@@ -9,108 +9,107 @@
     </b-row>
     <b-row>
       <b-col cols="12" md="8" offset-md="2">
-        <div class="p-4 bg-white rounded shadow-sm mb-4">
+        <div class="bg-white rounded shadow-sm mb-4">
           <b-form @submit.prevent="saveIssue()" autocomplete="off">
-            <b-row>
-              <b-col cols="12">
-                <b-form-group label="What is the type?">
-                  <b-form-select
-                    v-model="issue.type"
-                    :options="types"
-                    @input="getSubtypes()"
-                  >
-                    <template slot="first">
-                      <option :value="null" disabled
-                        >-- Please select an type --</option
-                      >
-                    </template>
-                  </b-form-select>
-                </b-form-group>
-              </b-col>
-              <b-col cols="12">
-                <b-form-group label="What is the subtype?">
-                  <b-form-select
-                    v-model="issue.subtype"
-                    :options="filteredSubtypes"
-                  >
-                    <template slot="first">
-                      <option :value="null" disabled
-                        >-- Please select an subtype --</option
-                      >
-                    </template>
-                  </b-form-select>
-                </b-form-group>
-              </b-col>
-              <b-col cols="12">
-                <b-tabs content-class="mt-3" justified lazy>
-                  <b-tab
-                    active
-                    title="Use My Location"
-                    @click="setLocationType('byCurrentLocation')"
-                  >
-                    <NavigatorGeolocationInput :location.sync="location" />
-                  </b-tab>
-                  <b-tab
-                    title="Use Address"
-                    @click="setLocationType('byAddress')"
-                  >
-                    <AddressGeocodeInput :location.sync="location" />
-                  </b-tab>
-                  <b-tab title="Use Map" @click="setLocationType('byMap')">
-                    <GoogleMapInput :location.sync="location" />
-                  </b-tab>
-                </b-tabs>
-              </b-col>
-              <b-col cols="12">
-                <b-form-group label="What is your email?">
-                  <b-form-checkbox v-model="anonymous">
-                    I'd prefer to stay anonymous
-                  </b-form-checkbox>
-                  <b-input
-                    type="email"
-                    v-model="issue.email"
-                    placeholder="johndoe@email.com"
-                    required
-                  />
-                </b-form-group>
-              </b-col>
-              <b-col cols="12">
-                <b-form-group label="Upload Image">
-                  <b-form-file
-                    type="file"
-                    ref="test"
-                    @change="onFileChange"
-                    v-model="file"
-                    placeholder="Choose file(s)..."
-                    drop-placeholder="Drop file(s) here..."
-                    multiple=""
-                  ></b-form-file>
-                </b-form-group>
-                <b-form-row>
-                  <b-col
-                    cols="3"
-                    v-for="(preview, index) in filePreview"
-                    :key="index"
-                  >
-                    <b-img thumbnail fluid :src="filePreview[index]" />
-                    <b-button variant="danger" @click="removeFile(index)"
-                      >Remove</b-button
+            <div class="p-4 border-bottom">
+              <h2 class="h4">Select an Issue</h2>
+              <b-form-group label="What is the type?">
+                <b-form-select
+                  v-model="issue.type"
+                  :options="types"
+                  @input="getSubtypes()"
+                >
+                  <template slot="first">
+                    <option :value="null" disabled
+                      >-- Please select an type --</option
                     >
-                  </b-col>
-                </b-form-row>
-              </b-col>
-              <b-col cols="12">
-                <b-form-group label="Additional Comments">
-                  <b-textarea
-                    class="form-control"
-                    v-model="issue.comments"
-                    rows="4"
-                    placeholder="Comments..."
-                    no-resize
-                  />
-                </b-form-group>
-              </b-col>
-              <b-col cols="12">
+                  </template>
+                </b-form-select>
+              </b-form-group>
+              <b-form-group label="What is the subtype?">
+                <b-form-select
+                  v-model="issue.subtype"
+                  :options="filteredSubtypes"
+                >
+                  <template slot="first">
+                    <option :value="null" disabled
+                      >-- Please select an subtype --</option
+                    >
+                  </template>
+                </b-form-select>
+              </b-form-group>
+            </div>
+            <div class="p-4 border-bottom">
+              <h2 class="h4">Select the Location</h2>
+              <b-tabs content-class="mt-3" justified lazy>
+                <b-tab
+                  active
+                  title="Use My Location"
+                  @click="setLocationType('byCurrentLocation')"
+                >
+                  <NavigatorGeolocationInput :location.sync="location" />
+                </b-tab>
+                <b-tab
+                  title="Use Address"
+                  @click="setLocationType('byAddress')"
+                >
+                  <AddressGeocodeInput :location.sync="location" />
+                </b-tab>
+                <b-tab title="Use Map" @click="setLocationType('byMap')">
+                  <GoogleMapInput :location.sync="location" />
+                </b-tab>
+              </b-tabs>
+            </div>
+            <div class="p-4 border-bottom">
+              <h2 class="h4">Add Your Email</h2>
+              <b-form-group label="What is your email?">
+                <b-input
+                  type="email"
+                  v-model="issue.email"
+                  placeholder=""
+                  required
+                  :disabled="issue.anonymous"
+                />
+                <b-form-checkbox v-model="issue.anonymous">
+                  I'd prefer to stay anonymous
+                </b-form-checkbox>
+              </b-form-group>
+            </div>
+            <div class="p-4">
+              <h2 class="h4">Add Additional Details</h2>
+              <b-form-group label="Upload Image">
+                <b-form-file
+                  type="file"
+                  ref="test"
+                  @change="onFileChange"
+                  v-model="file"
+                  placeholder="Choose file(s)..."
+                  drop-placeholder="Drop file(s) here..."
+                  multiple=""
+                ></b-form-file>
+              </b-form-group>
+              <b-form-row>
+                <b-col
+                  cols="3"
+                  v-for="(preview, index) in filePreview"
+                  :key="index"
+                >
+                  <b-img thumbnail fluid :src="filePreview[index]" />
+                  <b-button variant="danger" @click="removeFile(index)"
+                    >Remove</b-button
+                  >
+                </b-col>
+              </b-form-row>
+              <b-form-group label="Additional Comments">
+                <b-textarea
+                  class="form-control"
+                  v-model="issue.comments"
+                  rows="4"
+                  placeholder="Comments..."
+                  no-resize
+                />
+              </b-form-group>
+              <div>
                 <b-button
                   type="submit"
                   variant="primary"
@@ -121,8 +120,8 @@
                   </span>
                   <span v-else>Submit</span>
                 </b-button>
-              </b-col>
-            </b-row>
+              </div>
+            </div>
           </b-form>
         </div>
       </b-col>
@@ -159,9 +158,10 @@ export default {
         zipCode: null,
         neighborhood: null,
         locationType: "byCurrentLocation",
-        dateCreated: ""
+        dateCreated: "",
+        markerColor: "#0000EE",
+        anonymous: false
       },
-      anonymous: null,
       file: null,
       filePreview: [],
       types: typesJSON,
