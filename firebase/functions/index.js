d86eddab6f063ab283e8150cbb0ea7972a808fc1
IHIutch <jbhutch01@gmail.com>
2020-03-31 22:10:30 -0400

    Install and deploy sendgrid

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,27 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const domain = "beta.buffalo311.org";
+const fromEmail = `info@${domain}`;
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.submitThanks = functions.firestore
+  .document("/issues/{id}")
+  .onCreate((snap, context) => {
+    const isAnonymous = snap.data().anonymous;
+    if (!isAnonymous) {
+      const toEmail = snap.data().email;
+      const msg = {
+        to: toEmail,
+        from: fromEmail,
+        subject: "Sending with Twilio SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+      };
+      return sgMail.send(msg);
+    } else {
+      return null;
+    }
+  });
