7279a00224f3a24f4c271d838377616eb5e378b7
IHIutch <jbhutch01@gmail.com>
2020-05-02 23:14:39 -0400

    Remove unused dependencies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import BootstrapVue from "bootstrap-vue";
-import { firestorePlugin } from "vuefire";
 import * as Sentry from "@sentry/browser";
 import * as Integrations from "@sentry/integrations";
 
@@ -15,16 +14,15 @@ if (process.env.NODE_ENV === "production") {
   // Initialize Sentry (Live error tracking)
   Sentry.init({
     dsn: "https://fb0586e37cde4cbaa306926bf377ef5f@sentry.io/1516230",
-    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })],
   });
 }
 
-Vue.use(firestorePlugin);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
