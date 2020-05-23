99f213f59cd3216636dbe674ae4c2cadba0944a1
IHIutch <jbhutch01@gmail.com>
2020-05-22 22:42:32 -0400

    Update report page

--- src/pages/reports/_reportId.vue
+++ src/pages/reports/_reportId.vue
@@ -1,177 +1,199 @@
 <template>
   <div>
-    <b-container class="mt-5">
-      <b-row>
-        <b-col cols="12">
-          <b-row>
-            <b-col cols="12" md="9">
-              <b-row>
-                <b-col cols="12">
-                  <div class="d-flex mb-4 pb-4 border-bottom">
-                    <div>
-                      <div>
-                        <span class="text-muted">{{ report.type }}</span>
-                      </div>
-                      <div>
-                        <h1>
-                          {{ report.subtype }}
-                        </h1>
-                      </div>
-                      <div>
-                        <div class="d-flex align-items-center">
-                          <span class="mr-2">
-                            <map-pin-icon class="text-primary" />
-                          </span>
-                          <span class="text-muted">
-                            {{ report.lat | fixed3 }}, {{ report.lng | fixed3 }}
-                          </span>
-                          <span class="text-muted">
-                            {{ report.streetNum }} {{ report.streetName }}
-                            {{ report.zip }}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="ml-auto d-flex">
-                      <div class="mr-5">
-                        <div class="border rounded p-1 bg-white">
-                          <span class="h4">
-                            ID: {{ report.id | substr7 }}
-                          </span>
-                        </div>
-                      </div>
-                      <div>
-                        <div>
-                          <span
-                            class="d-flex text-uppercase small font-weight-bold"
-                          >
-                            Share
-                          </span>
-                        </div>
-                        <div class="btn btn-secondary">
-                          <i class="fab fa-facebook"></i>
-                        </div>
-                        <div class="btn btn-secondary">
-                          <i class="fab fa-twitter"></i>
-                        </div>
-                        <div class="btn btn-secondary">
-                          <i class="fas fa-link"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col cols="6">
-                  <div class="p-1 border rounded mb-4">
-                    <b-aspect
-                      :aspect="16 / 9"
-                      class="rounded-lg overflow-hidden"
-                    >
-                      <div id="map" class="embed-responsive-item"></div>
-                    </b-aspect>
-                  </div>
-                </b-col>
-                <b-col cols="6">
-                  <div>
-                    <div class="mb-2">
-                      <span class="font-weight-bold">Details</span>
-                    </div>
-                    <p>
-                      {{ report.comments }}
-                    </p>
-                  </div>
-                  <div>
-                    <div class="mb-2">
-                      <span class="font-weight-bold">Photos</span>
-                    </div>
-                    <b-form-row>
-                      <b-col
-                        cols="6"
-                        v-for="(image, index) in report.images"
-                        :key="index"
-                      >
-                        <b-button
-                          variant="link"
-                          class="p-0 mb-2 w-100"
-                          @click="showZoomImageModal(image)"
-                        >
-                          <div
-                            class="embed-responsive embed-responsive-1by1 rounded-lg overflow-hidden border"
-                          >
-                            <img
-                              class="embed-responsive-item object-cover"
-                              :src="image"
-                            />
-                          </div>
-                        </b-button>
-                      </b-col>
-                    </b-form-row>
-                  </div>
-                </b-col>
-              </b-row>
-            </b-col>
-            <b-col cols="12" md="3">
-              <div class="mb-4">
-                <div>
-                  <span class="d-flex text-dark small font-weight-bold">
-                    Status
-                  </span>
-                </div>
-                <span class="badge badge-pill">
-                  {{ report.status }}
-                </span>
-              </div>
-              <div class="mb-5">
-                <div>
-                  <span class="d-flex text-dark small font-weight-bold">
-                    Date Created
-                  </span>
-                </div>
-                <span>{{ report.dateCreated | date }}</span>
-              </div>
-              <div class="mb-5">
-                <div>
-                  <span class="d-flex text-dark small font-weight-bold">
-                    Date Assigned
-                  </span>
-                </div>
-                <template v-if="report.dateAssigned">
-                  <span>{{ report.dateAssigned | date }}</span>
-                </template>
-                <template v-else>
-                  <span class="text-muted small"><i>Not Assigned</i></span>
-                </template>
-              </div>
-              <div class="mb-5">
-                <div>
-                  <span class="d-flex text-dark small font-weight-bold">
-                    Date Scheduled
-                  </span>
-                </div>
-                <template v-if="report.dateScheduled">
-                  <span>{{ report.dateScheduled | date }}</span>
-                </template>
-                <template v-else>
-                  <span class="text-muted small"><i>Not scheduled</i></span>
-                </template>
-              </div>
+    <div class="px-4 pt-8 bg-white shadow-sm mb-12">
+      <b-container>
+        <b-row>
+          <b-col cols="10" offset="1">
+            <div class="d-flex align-items-end mb-12">
               <div>
-                <div>
-                  <span class="d-flex text-dark small font-weight-bold">
-                    Date Closed
-                  </span>
+                <div
+                  class="w-14 h-14 rounded-sm bg-primary mb-1 mr-4 d-flex align-items-center justify-content-center"
+                >
+                  <alert-triangle-icon size="32" class="text-white" />
                 </div>
-                <template v-if="report.closeDate">
-                  <span>{{ report.closeDate | date }}</span>
-                </template>
-                <template v-else>
-                  <span class="text-muted small"><i>Not Closed</i></span>
-                </template>
               </div>
-            </b-col>
-          </b-row>
+              <div class="flex-grow-1">
+                <div class="mb-4">
+                  <div class="px-2 py-1 border rounded-sm d-inline-flex">
+                    <span class="text-sm text-muted">
+                      Report: #<span class="text-uppercase"
+                        >{{ report.id | substr7 }}
+                      </span>
+                    </span>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="flex-grow-1">
+                    <div>
+                      <span class="d-flex text-sm text-muted">
+                        {{ report.type }}
+                      </span>
+                    </div>
+                    <div>
+                      <span class="d-flex text-3xl mt-n1">
+                        {{ report.subtype }}
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="bg-success px-2 py-1 rounded-sm">
+                      <span class="text-white text-lg">
+                        {{ report.status }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <b-form-group class="mb-0">
+                <b-form-radio-group
+                  id="displaySection"
+                  v-model="section.value"
+                  :options="section.options"
+                  buttons
+                  button-variant="outline-primary"
+                  name="displaySection"
+                ></b-form-radio-group>
+              </b-form-group>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <b-container>
+      <b-row>
+        <b-col cols="10" offset="1">
+          <div class="bg-white shadow-sm rounded-sm">
+            <b-row>
+              <b-col cols="9" class="border-right">
+                <div class="py-6 pl-6">
+                  <div class="mb-20">
+                    <div class="mb-2">
+                      <span class="font-weight-bold">Description:</span>
+                    </div>
+                    <template v-if="report.comments">
+                      <p class="mb-0">{{ report.comments }}</p>
+                    </template>
+                    <template v-else>
+                      <p class="text-muted mb-0">
+                        <i>No description provided.</i>
+                      </p>
+                    </template>
+                  </div>
+                  <b-row>
+                    <b-col>
+                      <div>
+                        <div class="mb-2">
+                          <span class="font-weight-bold">Location:</span>
+                        </div>
+                        <div class="mb-2">
+                          <div>
+                            <span v-if="report.streetNum" class="text-muted">
+                              {{ report.streetNum }}{{ report.streetName
+                              }}{{ report.zip }}
+                            </span>
+                          </div>
+                          <div>
+                            <span class="text-sm text-muted">
+                              {{ report.lat | fixed3 }},
+                              {{ report.lng | fixed3 }}
+                            </span>
+                          </div>
+                        </div>
+                        <div class="p-1 border rounded mb-4">
+                          <b-aspect
+                            :aspect="16 / 9"
+                            class="rounded-lg overflow-hidden"
+                          >
+                            <div id="map" class="embed-responsive-item"></div>
+                          </b-aspect>
+                        </div>
+                      </div>
+                    </b-col>
+                    <b-col>
+                      <div class="mb-2">
+                        <span class="font-weight-bold">Photos:</span>
+                      </div>
+                      <b-form-row>
+                        <b-col
+                          cols="6"
+                          v-for="(image, index) in report.images"
+                          :key="index"
+                        >
+                          <b-button
+                            variant="link"
+                            class="p-0 mb-2 w-100"
+                            @click="showZoomImageModal(image)"
+                          >
+                            <div
+                              class="embed-responsive embed-responsive-1by1 rounded-lg overflow-hidden border"
+                            >
+                              <img
+                                class="embed-responsive-item object-cover"
+                                :src="image"
+                              />
+                            </div>
+                          </b-button>
+                        </b-col>
+                      </b-form-row>
+                    </b-col>
+                  </b-row>
+                </div>
+              </b-col>
+              <b-col cols="3">
+                <div class="py-6 pr-6">
+                  <div class="mb-5">
+                    <div>
+                      <span class="d-flex text-dark small font-weight-bold">
+                        Date Created
+                      </span>
+                    </div>
+                    <span>{{ report.dateCreated | date }}</span>
+                  </div>
+                  <div class="mb-5">
+                    <div>
+                      <span class="d-flex text-dark small font-weight-bold">
+                        Date Assigned
+                      </span>
+                    </div>
+                    <template v-if="report.dateAssigned">
+                      <span>{{ report.dateAssigned | date }}</span>
+                    </template>
+                    <template v-else>
+                      <span class="text-muted small"><i>Not Assigned</i></span>
+                    </template>
+                  </div>
+                  <div class="mb-5">
+                    <div>
+                      <span class="d-flex text-dark small font-weight-bold">
+                        Date Scheduled
+                      </span>
+                    </div>
+                    <template v-if="report.dateScheduled">
+                      <span>{{ report.dateScheduled | date }}</span>
+                    </template>
+                    <template v-else>
+                      <span class="text-muted small"><i>Not scheduled</i></span>
+                    </template>
+                  </div>
+                  <div>
+                    <div>
+                      <span class="d-flex text-dark small font-weight-bold">
+                        Date Closed
+                      </span>
+                    </div>
+                    <template v-if="report.closeDate">
+                      <span>{{ report.closeDate | date }}</span>
+                    </template>
+                    <template v-else>
+                      <span class="text-muted small"><i>Not Closed</i></span>
+                    </template>
+                  </div>
+                </div>
+              </b-col>
+            </b-row>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -195,13 +217,13 @@
 
 <script>
 import dayjs from "dayjs";
-import { MapPinIcon } from "vue-feather-icons";
+import { AlertTriangleIcon } from "vue-feather-icons";
 
 export default {
   name: "ReportPage",
   layout: "PublicLayout",
   components: {
-    MapPinIcon
+    AlertTriangleIcon
   },
   head() {
     const reportId = this.substr7(this.report.id).toUpperCase();
@@ -264,7 +286,14 @@ export default {
   data() {
     return {
       imageZoomUrl: null,
-      report: []
+      report: {},
+      section: {
+        value: "overview",
+        options: [
+          { text: "Overview", value: "overview" },
+          { text: "Updates", value: "updates" }
+        ]
+      }
     };
   },
   methods: {
