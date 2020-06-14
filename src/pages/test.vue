89d234d4187bd647d22a880a996876c9b5183ae0
IHIutch <jbhutch01@gmail.com>
2020-06-14 16:01:56 -0400

    Layout updates

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,216 +1,203 @@
 <template>
   <b-container tag="main" role="main" class="pt-14 vh-100">
     <b-row class="h-100 py-5">
-      <b-col md="6" offset-md="3" class="h-100 position-relative">
-        <transition :name="slideDirection">
-          <template v-if="step == 1">
-            <div
-              key="1"
-              class="bg-white rounded shadow-sm h-100 w-100 overflow-hidden position-absolute d-flex flex-column p-4"
-            >
-              <div class="mb-4">
-                <div>
-                  <h1 class="text-2xl">Select a Report Type</h1>
-                </div>
-                <div>
-                  <b-form-group
-                    class="mb-0"
-                    :description="
-                      `Example: &quot;${searchExamples[0]}&quot; or &quot;${searchExamples[1]}&quot;`
-                    "
-                  >
-                    <div class="position-relative">
-                      <b-form-input
-                        class="pl-10"
-                        placeholder="Search..."
-                        v-model="search"
-                        type="text"
-                      ></b-form-input>
-                      <div
-                        class="position-absolute top-0 left-0 h-100 w-10 d-flex align-items-center justify-content-center"
-                      >
-                        <search-icon />
-                      </div>
-                    </div>
-                  </b-form-group>
-                </div>
-              </div>
-              <div class="d-flex flex-grow-1 overflow-auto border rounded">
-                <b-list-group class="w-100" flush>
-                  <div v-for="(group, gIdx) in groups">
-                    <b-list-group-item
-                      :key="`group-${gIdx}`"
-                      class="sticky-top text-uppercase font-weight-bold bg-secondary text-white px-2 py-1 text-sm border-left-0 border-right-0"
-                    >
-                      {{ group.title }}
-                    </b-list-group-item>
-                    <b-list-group-item
-                      v-for="(issue, iIdx) in group.issues"
-                      :key="`issue-${iIdx}`"
-                      class="border-left-0 border-right-0 d-flex align-items-center justify-content-between text-primary"
-                      button
-                      @click="selectType(group.title, issue)"
-                    >
-                      <span>{{ issue }}</span>
-                      <chevron-right-icon />
-                    </b-list-group-item>
-                  </div>
-                </b-list-group>
-              </div>
-            </div>
-          </template>
-          <template v-else>
-            <div
-              key="2"
-              class="bg-white rounded shadow-sm h-100 w-100 overflow-auto position-absolute"
-            >
-              <div class="embed-responsive embed-responsive-21by9">
-                <div
-                  class="embed-responsive-item bg-secondary d-flex flex-column justify-content-between p-6"
-                >
+      <b-col md="6" offset-md="3" class="h-100">
+        <div class="h-100 position-relative">
+          <transition :name="slideDirection">
+            <template v-if="step == 1">
+              <div
+                key="1"
+                class="bg-white rounded shadow-sm h-100 w-100 overflow-hidden position-absolute d-flex flex-column p-4"
+              >
+                <div class="mb-4">
                   <div>
-                    <b-button
-                      variant="link-white"
-                      class="border-0 d-flex align-items-center p-1 text-decoration-none"
-                      @click="goBack()"
-                    >
-                      <span class="d-flex">
-                        <chevron-left-icon size="22" />
-                      </span>
-                      <span class="ml-1">Back</span></b-button
-                    >
+                    <h1 class="text-2xl">Select a Report Type</h1>
                   </div>
                   <div>
-                    <div>
-                      <span class="text-white">{{ form.issueGroup }}</span>
-                    </div>
-                    <div>
-                      <span class="text-white text-3xl">
-                        {{ form.issueType }}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div class="d-flex align-items-start px-4 py-2 border-bottom">
-                  <div
-                    class="w-10 h-10 d-flex align-items-center justify-content-center"
-                  >
-                    <map-pin-icon />
-                  </div>
-                  <div class="flex-grow-1 pl-4">
-                    <div
-                      class="d-flex align-items-center justify-content-between mb-2"
-                    >
-                      <div class="d-flex">
-                        <span class="font-weight-bold text-sm">Location</span>
-                        <span class="text-muted text-sm pl-1">(Required)</span>
-                      </div>
-                      <div>
-                        <b-form-group class="mb-0">
-                          <b-form-radio-group
-                            id="btn-radios-2"
-                            v-model="map.selected"
-                            :options="map.options"
-                            buttons
-                            button-variant="outline-primary"
-                            size="sm"
-                            name="radio-btn-outline"
-                          ></b-form-radio-group>
-                        </b-form-group>
-                      </div>
-                    </div>
-                    <div>
-                      <template v-if="map.selected == 'map'">
-                        <div class="mb-2">
-                          <GoogleMapInput :location.sync="location" />
-                        </div>
-                        <div>
-                          <b-form-group
-                            description="Click a point on the map or use your current location"
-                          >
-                            <b-input-group>
-                              <b-form-input readonly></b-form-input>
-                              <b-input-group-append>
-                                <b-button
-                                  variant="primary"
-                                  class="py-0 align-items-center d-flex"
-                                >
-                                  <crosshair-icon size="20" />
-                                </b-button>
-                              </b-input-group-append>
-                            </b-input-group>
-                          </b-form-group>
-                        </div>
-                      </template>
-                      <template v-else>
-                        <AddressGeocodeInput
-                          label="Search for an address"
-                          description="Select an option from the dropdown"
-                          :location.sync="location"
-                        />
-                      </template>
-                    </div>
-                  </div>
-                </div>
-                <div class="d-flex align-items-start px-4 py-2 border-bottom">
-                  <div
-                    class="w-10 h-10 d-flex align-items-center justify-content-center"
-                  >
-                    <camera-icon />
-                  </div>
-                  <div class="flex-grow-1 pl-4">
-                    <div class="d-flex mb-2">
-                      <span class="font-weight-bold text-sm">Photos</span>
-                    </div>
-                    <div class="row">
-                      <div class="col-3">
-                        <div class="embed-responsive embed-responsive-1by1">
-                          <button
-                            type="button"
-                            class="embed-responsive-item bg-light border border-dark d-flex align-items-center justify-content-center rounded"
-                          >
-                            <div>
-                              <div>
-                                <plus-icon />
-                              </div>
-                              <div><span>Add</span></div>
-                            </div>
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="d-flex align-items-start px-4 py-2 border-bottom">
-                  <div
-                    class="w-10 h-10 d-flex align-items-center justify-content-center"
-                  >
-                    <edit-icon />
-                  </div>
-                  <div class="flex-grow-1 pl-4">
                     <b-form-group
-                      label-class="font-weight-bold text-sm"
-                      label="Description/Details"
-                      label-for="textarea"
+                      class="mb-0"
+                      :description="
+                        `Example: &quot;${searchExamples[0]}&quot; or &quot;${searchExamples[1]}&quot;`
+                      "
                     >
-                      <b-form-textarea
-                        id="textarea"
-                        placeholder="Additional Details"
-                        rows="6"
-                        max-rows="8"
-                      ></b-form-textarea>
+                      <div class="position-relative">
+                        <b-form-input
+                          class="pl-10"
+                          placeholder="Search..."
+                          v-model="search"
+                          type="text"
+                        ></b-form-input>
+                        <div
+                          class="position-absolute top-0 left-0 h-100 w-10 d-flex align-items-center justify-content-center"
+                        >
+                          <search-icon />
+                        </div>
+                      </div>
                     </b-form-group>
                   </div>
                 </div>
+                <div class="d-flex flex-grow-1 overflow-auto border rounded">
+                  <b-list-group class="w-100" flush>
+                    <div v-for="(group, gIdx) in groups">
+                      <b-list-group-item
+                        :key="`group-${gIdx}`"
+                        class="sticky-top text-uppercase font-weight-bold bg-secondary text-white px-2 py-1 text-sm border-left-0 border-right-0"
+                      >
+                        {{ group.title }}
+                      </b-list-group-item>
+                      <b-list-group-item
+                        v-for="(issue, iIdx) in group.issues"
+                        :key="`issue-${iIdx}`"
+                        class="border-left-0 border-right-0 d-flex align-items-center justify-content-between text-primary"
+                        button
+                        @click="selectType(group.title, issue)"
+                      >
+                        <span>{{ issue }}</span>
+                        <chevron-right-icon />
+                      </b-list-group-item>
+                    </div>
+                  </b-list-group>
+                </div>
               </div>
-              <div class="p-4">
-                <b-button variant="primary" block>Submit</b-button>
+            </template>
+            <template v-else>
+              <div
+                key="2"
+                class="bg-white rounded shadow-sm h-100 w-100 overflow-auto position-absolute"
+              >
+                <div class="embed-responsive embed-responsive-21by9">
+                  <div
+                    class="embed-responsive-item bg-secondary d-flex flex-column justify-content-between p-6"
+                  >
+                    <div>
+                      <b-button
+                        variant="link-white"
+                        class="border-0 d-flex align-items-center p-1 text-decoration-none"
+                        @click="goBack()"
+                      >
+                        <span class="d-flex">
+                          <chevron-left-icon size="22" />
+                        </span>
+                        <span class="ml-1">Back</span></b-button
+                      >
+                    </div>
+                    <div>
+                      <div>
+                        <span class="text-white">{{ form.issueGroup }}</span>
+                      </div>
+                      <div>
+                        <span class="text-white text-3xl">
+                          {{ form.issueType }}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                    <div
+                      class="w-10 h-10 d-flex align-items-center justify-content-center"
+                    >
+                      <map-pin-icon />
+                    </div>
+                    <div class="flex-grow-1 pl-4">
+                      <div
+                        class="d-flex align-items-center justify-content-between mb-2 flex-wrap"
+                      >
+                        <div class="d-flex">
+                          <span class="font-weight-bold text-sm">Location</span>
+                          <span class="text-muted text-sm pl-1"
+                            >(Required)</span
+                          >
+                        </div>
+                        <div>
+                          <b-form-group class="mb-0">
+                            <b-form-radio-group
+                              id="btn-radios-2"
+                              v-model="map.selected"
+                              :options="map.options"
+                              buttons
+                              button-variant="outline-primary"
+                              size="sm"
+                              name="radio-btn-outline"
+                            ></b-form-radio-group>
+                          </b-form-group>
+                        </div>
+                      </div>
+                      <div>
+                        <template v-if="map.selected == 'map'">
+                          <div class="mb-2">
+                            <GoogleMapInput :location.sync="location" />
+                          </div>
+                        </template>
+                        <template v-else>
+                          <AddressGeocodeInput
+                            label="Search for an address"
+                            description="Select an option from the dropdown"
+                            :location.sync="location"
+                          />
+                        </template>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                    <div
+                      class="w-10 h-10 d-flex align-items-center justify-content-center"
+                    >
+                      <camera-icon />
+                    </div>
+                    <div class="flex-grow-1 pl-4">
+                      <div class="d-flex mb-2">
+                        <span class="font-weight-bold text-sm">Photos</span>
+                      </div>
+                      <b-row>
+                        <b-col cols="6" md="4">
+                          <div class="embed-responsive embed-responsive-1by1">
+                            <button
+                              type="button"
+                              class="embed-responsive-item bg-light border border-dark d-flex align-items-center justify-content-center rounded"
+                            >
+                              <div>
+                                <div>
+                                  <plus-icon />
+                                </div>
+                                <div><span>Add</span></div>
+                              </div>
+                            </button>
+                          </div>
+                        </b-col>
+                      </b-row>
+                    </div>
+                  </div>
+                  <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                    <div
+                      class="w-10 h-10 d-flex align-items-center justify-content-center"
+                    >
+                      <edit-icon />
+                    </div>
+                    <div class="flex-grow-1 pl-4">
+                      <b-form-group
+                        label-class="font-weight-bold text-sm"
+                        label="Description/Details"
+                        label-for="textarea"
+                      >
+                        <b-form-textarea
+                          id="textarea"
+                          placeholder="Additional Details"
+                          rows="6"
+                          max-rows="8"
+                        ></b-form-textarea>
+                      </b-form-group>
+                    </div>
+                  </div>
+                </div>
+                <div class="p-4">
+                  <b-button variant="primary" block>Submit</b-button>
+                </div>
               </div>
-            </div>
-          </template>
-        </transition>
+            </template>
+          </transition>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -228,8 +215,7 @@ import {
   MapPinIcon,
   CameraIcon,
   EditIcon,
-  PlusIcon,
-  CrosshairIcon
+  PlusIcon
 } from "vue-feather-icons";
 
 export default {
@@ -243,7 +229,6 @@ export default {
     CameraIcon,
     EditIcon,
     PlusIcon,
-    CrosshairIcon,
     AddressGeocodeInput,
     GoogleMapInput
   },
@@ -266,7 +251,7 @@ export default {
             value: "map"
           },
           {
-            text: "Search Address",
+            text: "Use Address",
             value: "address"
           }
         ]
