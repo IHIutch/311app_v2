658188ce95c7a20cdd6593370ff1e8d0a36e2581
IHIutch <jbhutch01@gmail.com>
2020-06-14 12:35:49 -0400

    Mock up new report process

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -49,7 +49,7 @@
                       :key="`issue-${iIdx}`"
                       class="border-left-0 border-right-0 d-flex align-items-center justify-content-between text-primary"
                       button
-                      @click="selectType(issue)"
+                      @click="selectType(group.title, issue)"
                     >
                       <span>{{ issue }}</span>
                       <chevron-right-icon />
@@ -62,19 +62,157 @@
           <template v-else>
             <div
               key="2"
-              class="bg-white rounded shadow-sm h-100 w-100 overflow-hidden position-absolute"
+              class="bg-white rounded shadow-sm h-100 w-100 overflow-auto position-absolute"
             >
               <div class="embed-responsive embed-responsive-21by9">
                 <div
-                  class="embed-responsive-item bg-secondary d-flex align-items-end"
+                  class="embed-responsive-item bg-secondary d-flex flex-column justify-content-between p-6"
                 >
-                  <div class="p-2">
-                    <span class="text-white">{{ form.issueType }}</span>
+                  <div>
+                    <b-button
+                      variant="link-white"
+                      class="border-0 d-flex align-items-center p-1 text-decoration-none"
+                      @click="goBack()"
+                    >
+                      <span class="d-flex">
+                        <chevron-left-icon size="22" />
+                      </span>
+                      <span class="ml-1">Back</span></b-button
+                    >
+                  </div>
+                  <div>
+                    <div>
+                      <span class="text-white">{{ form.issueGroup }}</span>
+                    </div>
+                    <div>
+                      <span class="text-white text-3xl">
+                        {{ form.issueType }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                  <div
+                    class="w-10 h-10 d-flex align-items-center justify-content-center"
+                  >
+                    <map-pin-icon />
+                  </div>
+                  <div class="flex-grow-1 pl-4">
+                    <div
+                      class="d-flex align-items-center justify-content-between mb-2"
+                    >
+                      <div class="d-flex">
+                        <span class="font-weight-bold text-sm">Location</span>
+                        <span class="text-muted text-sm pl-1">(Required)</span>
+                      </div>
+                      <div>
+                        <b-form-group class="mb-0">
+                          <b-form-radio-group
+                            id="btn-radios-2"
+                            v-model="map.selected"
+                            :options="map.options"
+                            buttons
+                            button-variant="outline-primary"
+                            size="sm"
+                            name="radio-btn-outline"
+                          ></b-form-radio-group>
+                        </b-form-group>
+                      </div>
+                    </div>
+                    <div>
+                      <template v-if="map.selected == 'map'">
+                        <div
+                          class="embed-responsive embed-responsive-21by9 mb-4"
+                        >
+                          <div
+                            class="embed-responsive-item bg-secondary rounded"
+                          ></div>
+                        </div>
+                        <div>
+                          <b-form-group
+                            description="Click a point on the map or use your current location"
+                          >
+                            <b-input-group>
+                              <b-form-input></b-form-input>
+                              <b-input-group-append>
+                                <b-button
+                                  variant="primary"
+                                  class="py-0 align-items-center d-flex"
+                                >
+                                  <crosshair-icon size="20" />
+                                </b-button>
+                              </b-input-group-append>
+                            </b-input-group>
+                          </b-form-group>
+                        </div>
+                      </template>
+                      <template v-else>
+                        <b-form-group
+                          label="Search by address"
+                          label-for="input-1"
+                          description="Type to see options"
+                        >
+                          <b-form-input id="input-1" type="text"></b-form-input>
+                        </b-form-group>
+                      </template>
+                    </div>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                  <div
+                    class="w-10 h-10 d-flex align-items-center justify-content-center"
+                  >
+                    <camera-icon />
+                  </div>
+                  <div class="flex-grow-1 pl-4">
+                    <div class="d-flex mb-2">
+                      <span class="font-weight-bold text-sm">Photos</span>
+                    </div>
+                    <div class="row">
+                      <div class="col-3">
+                        <div class="embed-responsive embed-responsive-1by1">
+                          <button
+                            type="button"
+                            class="embed-responsive-item bg-light border border-dark d-flex align-items-center justify-content-center rounded"
+                          >
+                            <div>
+                              <div>
+                                <plus-icon />
+                              </div>
+                              <div><span>Add</span></div>
+                            </div>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start px-4 py-2 border-bottom">
+                  <div
+                    class="w-10 h-10 d-flex align-items-center justify-content-center"
+                  >
+                    <edit-icon />
+                  </div>
+                  <div class="flex-grow-1 pl-4">
+                    <b-form-group
+                      label-class="font-weight-bold text-sm"
+                      label="Description/Details"
+                      label-for="textarea"
+                    >
+                      <b-form-textarea
+                        id="textarea"
+                        placeholder="Additional Details"
+                        rows="6"
+                        max-rows="8"
+                      ></b-form-textarea>
+                    </b-form-group>
                   </div>
                 </div>
               </div>
               <div class="p-4">
-                <b-button @click="goBack()">Back</b-button>
+                <b-button variant="primary" block>Submit</b-button>
               </div>
             </div>
           </template>
@@ -86,14 +224,29 @@
 
 <script>
 import issuesJSON from "@/data/issues.json";
-import { ChevronRightIcon, SearchIcon } from "vue-feather-icons";
+import {
+  ChevronRightIcon,
+  ChevronLeftIcon,
+  SearchIcon,
+  MapPinIcon,
+  CameraIcon,
+  EditIcon,
+  PlusIcon,
+  CrosshairIcon
+} from "vue-feather-icons";
 
 export default {
   name: "test",
   layout: "PublicLayout",
   components: {
     ChevronRightIcon,
-    SearchIcon
+    ChevronLeftIcon,
+    SearchIcon,
+    MapPinIcon,
+    CameraIcon,
+    EditIcon,
+    PlusIcon,
+    CrosshairIcon
   },
   data() {
     return {
@@ -102,16 +255,30 @@ export default {
       search: "",
       searchExamples: [],
       form: {
+        issueGroup: "",
         issueType: ""
+      },
+      map: {
+        selected: "map",
+        options: [
+          {
+            text: "Use Map",
+            value: "map"
+          },
+          {
+            text: "Search Address",
+            value: "address"
+          }
+        ]
       },
       step: 1
     };
   },
   methods: {
-    selectType(val) {
-      this.form.issueType = val;
-      this.step = 2;
-      this.slideDirection = "slide-fade-forward";
+    selectType(group, issue) {
+      this.form.issueGroup = group;
+      this.form.issueType = issue;
+      this.goForward();
     },
     getSearchExamples() {
       while (this.searchExamples.length < 3) {
@@ -122,8 +289,12 @@ export default {
       }
     },
     goBack() {
-      this.step = 1;
+      this.step--;
       this.slideDirection = "slide-fade-back";
+    },
+    goForward() {
+      this.step++;
+      this.slideDirection = "slide-fade-forward";
     }
   },
   created() {
