28e4760a81632d5dd6b4bc2e7ce4cdb835cf24ee
IHIutch <jbhutch01@gmail.com>
2020-07-29 20:24:59 -0400

    Define some constants

---
+++ constants/reportStatus.js
@@ -0,0 +1,6 @@
+export const reportStatus = Object.freeze({
+  OPEN: 1,
+  ASSIGNED: 2,
+  SCHEDULED: 3,
+  CLOSED: 4
+});
---
+++ constants/userType.js
@@ -0,0 +1,5 @@
+export const userType = Object.freeze({
+  USER: 1,
+  ADMIN: 2,
+  SUPER: 3
+});
