41c7d610cf4cf3aa7c4bd53aeebbec00034f90f3
Jonathan Hutchison <jh@helmux.com>
2020-12-27 15:14:45 -0500

    Get mjml templates working

--- api/v1/routes/users.js
+++ api/v1/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import sgMail from '@sendgrid/mail'
+import renderEmail from '../../../emails'
 import { User } from '../models/index'
 
 const router = express.Router()
@@ -12,24 +13,26 @@ router.post('/', (req, res) => {
     password,
   })
     .then((data) => {
-      const msg = {
-        to: data.email,
-        from: 'jbhutch01+311@gmail.com', // Use the email address or domain you verified above
-        subject: 'Sending with Twilio SendGrid is Fun',
-        text: 'This is a test',
-        html: '<strong>This is a test</strong>',
-      }
-      sgMail.send(msg).then(
-        () => {
-          res.status(201).json(data.id)
-        },
-        (err) => {
-          console.error(err)
-          if (err.response) {
-            console.error(err.response.body)
-          }
+      renderEmail('welcome', { email }).then(({ text, html }) => {
+        const msg = {
+          to: data.email,
+          from: 'jbhutch01+311@gmail.com', // Use the email address or domain you verified above
+          subject: 'Sending with Twilio SendGrid is Fun',
+          text,
+          html,
         }
-      )
+        sgMail.send(msg).then(
+          () => {
+            res.status(201).json(data.id)
+          },
+          (err) => {
+            console.error(err)
+            if (err.response) {
+              console.error(err.response.body)
+            }
+          }
+        )
+      })
     })
     .catch((err) => console.log(err))
 })
---
+++ emails/index.js
@@ -0,0 +1,39 @@
+import FS from 'fs'
+import VUE from 'vue'
+import mjml2html from 'mjml'
+import { htmlToText } from 'html-to-text'
+import { createRenderer } from 'vue-server-renderer'
+
+const renderEmail = (templateName, data) => {
+  return new Promise((resolve, reject) => {
+    const filePath = `./emails/templates/${templateName}.mjml`
+    if (FS.existsSync(filePath)) {
+      const app = new VUE({
+        data,
+        template: FS.readFileSync(filePath, 'utf-8'),
+      })
+      createRenderer().renderToString(app, (err, vueRender) => {
+        if (err) {
+          throw new Error(`Error at Vue compiler: ${err}`)
+        } else {
+          const htmlOutput = mjml2html(vueRender).html
+          const textOutput = htmlToText(htmlOutput)
+          if (htmlOutput && textOutput) {
+            resolve({
+              text: textOutput,
+              html: htmlOutput,
+            })
+          } else {
+            reject(err)
+          }
+        }
+      })
+    } else {
+      throw new Error(
+        `Could not find template: ./templates/${templateName}.mjml`
+      )
+    }
+  })
+}
+
+export default renderEmail
