6bb187f84b39b945ebd8c96fcb3115c4d02fe654
Jonathan Hutchison <jh@helmux.com>
2020-12-30 13:59:09 -0500

    Update nav

--- components/common/PublicNav.vue
+++ components/common/PublicNav.vue
@@ -1,6 +1,10 @@
 <template>
   <b-navbar type="light" variant="white" fixed="top" class="shadow-sm h-14">
     <b-navbar-brand to="/"> Buffalo 311 </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item to="/" class="mr-2">Reports</b-nav-item>
+      <b-nav-item to="/alerts" class="mr-2">Alerts</b-nav-item>
+    </b-navbar-nav>
     <b-navbar-nav class="ml-auto">
       <b-nav-item to="/register" class="mr-2">Register</b-nav-item>
       <b-nav-item to="/log-in" class="mr-2">Log In</b-nav-item>
